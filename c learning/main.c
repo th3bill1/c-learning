0bd7b9f935177c1730d5549c6469fca5d00d62ef
Wojciech Wójcik <wojciech.wojcik@k-net.pl>
2022-10-06 13:21:37 +0200

    more tasks, set1

--- c learning/main.c
+++ c learning/main.c
@@ -5,60 +5,107 @@
 
 int main(void)
 {
-	int chosenSet, chosenTask;
-	printf("Put the number of exercise set: ");
-	if (scanf("%i", &chosenSet) == 1 && chosenSet > 0 && chosenSet < 9)
+	int chosenTask;
+	
+	int programActive = 1;
+	while (programActive)
 	{
-		switch (chosenSet)
+		printf("Put the number of exercise (if you want to leave put 0): ");
+		if (scanf("%i", &chosenTask) == 1 && chosenTask > -1 && chosenTask < 18)
 		{
-		case 1:
-			printf("Put the number of exercise: ");
-			if (scanf("%i", &chosenTask) == 1 && chosenTask > 0 && chosenSet < 18)
+			switch (chosenTask)
 			{
-				switch (chosenTask)
+			case 0:
+				programActive = 0;
+				break;
+			case 1:
+				double side, perimeter, area;
+				printf("Put the side length of your square: ");
+				if (scanf("%lf", &side) == 1)
 				{
-				case 1:
-					double side, perimeter, area;
-					printf("Put the side length of your square: ");
-					if (scanf("%lf", &side) == 1)
+					if (side > 0)
 					{
-						if (side > 0)
-						{
-							perimeter = side * 4;
-							printf("\nPerimeter equales: %lf", perimeter);
-							area = side * side;
-							printf("\nArea equales: %lf", area);
-							printf("\n");
-						}
-						else printf("Side length must be a positive number.");
+						perimeter = side * 4;
+						printf("\nPerimeter equales: %lf", perimeter);
+						area = side * side;
+						printf("\nArea equales: %lf\n", area);
+						printf("\n");
 					}
-					else printf("Wrong input!");
-					break;
-				case 2:
-					double radius, perimeter2, area2;
-					printf("Put the radius of your circle: ");
-					if (scanf("%lf", &radius) == 1)
-					{
-						if (radius > 0)
-						{
-							perimeter2 = 2 * radius * M_PI;
-							printf("\nPerimeter equales: %lf", perimeter2);
-							area2 = radius * radius * M_PI;
-							printf("\nArea equales: %lf", area2);
-							printf("\n");
-						}
-						else printf("Radius of the circle must be a positive number.");
-					}
-					break;
+					else printf("Side length must be a positive number.");
 				}
+				else printf("Wrong input!");
+				break;
+			case 2:
+				double radius, perimeter2, area2;
+				printf("Put the radius of your circle: ");
+				if (scanf("%lf", &radius) == 1)
+				{
+					if (radius > 0)
+					{
+						perimeter2 = 2 * radius * M_PI;
+						printf("\nPerimeter equales: %lf", perimeter2);
+						area2 = radius * radius * M_PI;
+						printf("\nArea equales: %lf\n", area2);
+					}
+					else printf("Radius of the circle must be a positive number.");
+				}
+				break;
+			case 3:
+				int integer1, integer2, quotient, reminder;
+				double doubleQuotient;
+				printf("Put the two numbers you want to divide: \n");
+				if (scanf("%d%d", &integer1, &integer2) == 2)
+				{
+					if (integer2 != 0)
+					{
+						quotient = integer1 / integer2;
+						reminder = integer1 - quotient * integer2;
+						doubleQuotient = (double)integer1 / integer2;
+						printf("Quotient is equal to %lf\n", doubleQuotient);
+						printf("Reminder is equal to %d\n", reminder);
+					}
+					else printf("You cannot divide by 0");
+				}
+				else printf("Wrong input.");
+				break;
+			case 4:
+				const int temperature = 27;
+				double temperature2;
+				temperature2 = (double)(temperature - 32) / 1.8;
+				printf("Temperature = %lf", temperature2);
+				break;
+			case 5:
+				const double conv = 1.61;
+				double kilometers, miles;
+				printf("Put the distance in kilometers: ");
+				if (scanf("%lf", &kilometers) == 1)
+				{
+					if (kilometers > 0)
+					{
+						miles = kilometers / conv;
+						printf("%lf kilometers is equal to %lf miles.", kilometers, miles);
+					}
+					else printf("Distance must be a positive number.");
+				}
+				else printf("Wrong input.");
+				break;
+			case 6:
+				int number;
+				printf("Put your number: ");
+				if (scanf("%d", &number)==1)
+				{
+					if (number % 2 == 1) printf("This number is odd\n");
+					else printf("This number is even\n");
+				}
+				else printf("Input must be an integer.");
+				break;
 			}
-			else printf("There is no such task. There is 18 tasks avaliable.\n");
-			break;
-		case 2:
-			break;
 		}
-
+		else
+		{
+			printf("There is no such task. There is 18 tasks avaliable.\n");
+			chosenTask = -1;
+		}
 	}
-	else printf("There is no such set. There are 8 sets avaliable.");
 	return 0;
 }
